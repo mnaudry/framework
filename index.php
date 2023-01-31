c3c89b9bb131bad856a51d2502a31aa508fe3b6c
mnaudry <audrymakampila@gmail.com>
2023-01-30 21:29:27 -0400

    template function

--- index.php
+++ index.php
@@ -17,29 +17,31 @@ $context = new RequestContext();
 $context = $context->fromRequest($request);
 
 
-$response = new Response();
+//$response = new Response();
 
 $routes = include __DIR__."/template/routes.php";
 
 $matcher = new UrlMatcher($routes,$context);
 
+
 try {
 
-    extract($matcher->match($path),EXTR_SKIP);
-    ob_start();
-    include __DIR__.sprintf("/template/pages/%s.php",$_route);
-    $response->setContent(ob_get_clean());
+    //extract($matcher->match($path),EXTR_SKIP);
+   // ob_start();
+   // include __DIR__.sprintf("/template/pages/%s.php",$_route);
+   // $response = new Response(ob_get_clean());
+   $request->attributes->add($matcher->match($path));
+
+   $response = call_user_func($request->attributes->get('_controller'),$request);
 
 }catch(Exception $e) {
     if($e instanceof RouteNotFoundException){
-        $response->setContent('Not found');
-        $response->setStatusCode(404);
+        $response = new Response('Not found',404);
     } else {
-        $response->setContent('Server error');
-        $response->setStatusCode(500);
+
+        $response = new Response('Server error',500);
     }
 }
-
 
 
 $response->send();
--- template/routes.php
+++ template/routes.php
@@ -1,11 +1,22 @@
 <?php
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+function render_template($request){
+
+    ob_start();
+    extract($request->attributes->all(),EXTR_SKIP);
+    include __DIR__.sprintf("/../template/pages/%s.php",$_route);
+
+    return new Response(ob_get_clean());
+}
 
 $routes = new RouteCollection();
-$routes->add('hello',new Route('/hello/{name}',['name' => 'audry']));
-$routes->add('goodbye',new Route('/goodbye'));
-$routes->add('home',new Route('/'));
+$routes->add('hello',new Route('/hello/{name}',['name' => 'audry','_controller' =>  'render_template']
+));
+$routes->add('goodbye',new Route('/goodbye',['_controller' => 'render_template']));
+$routes->add('home',new Route('/',['_controller' => 'render_template' ]));
 
 
 return $routes;
