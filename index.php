39bfa5198f0a538ca6110b6b639d988c7cc28ef6
Audry Makampila <audry.makampila@voxinteractif.com>
2023-01-31 10:48:17 -0400

    correct some bugs

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;   
-use Symfony\Component\Routing\Exception\RouteNotFoundException;    
+use Symfony\Component\Routing\Exception\RouteNotFoundException; 
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;  
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver; 
 
 
 if(!isset($request)) {
@@ -16,8 +18,9 @@ $path = $request->getPathInfo();
 $context = new RequestContext();
 $context = $context->fromRequest($request);
 
+$contollerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
 
-//$response = new Response();
 
 $routes = include __DIR__."/template/routes.php";
 
@@ -25,20 +28,16 @@ $matcher = new UrlMatcher($routes,$context);
 
 
 try {
+    $request->attributes->add($matcher->match($path));
 
-    //extract($matcher->match($path),EXTR_SKIP);
-   // ob_start();
-   // include __DIR__.sprintf("/template/pages/%s.php",$_route);
-   // $response = new Response(ob_get_clean());
-   $request->attributes->add($matcher->match($path));
-
-   $response = call_user_func($request->attributes->get('_controller'),$request);
+    $controller = $contollerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request,$controller);
+    $response = call_user_func_array($controller,$arguments );
 
 }catch(Exception $e) {
     if($e instanceof RouteNotFoundException){
         $response = new Response('Not found',404);
     } else {
-
         $response = new Response('Server error',500);
     }
 }
--- template/routes.php
+++ template/routes.php
@@ -2,13 +2,13 @@
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\HttpFoundation\Response;
-use App\Controller\NumberController;
+use Symfony\Component\HttpFoundation\Request;
 
-function render_template($request){
+function render_template(Request $request ){
 
     ob_start();
     extract($request->attributes->all(),EXTR_SKIP);
-    include __DIR__.sprintf("/../template/pages/%s.php",$_route);
+    include __DIR__.sprintf("/pages/%s.php",$_route);
 
     return new Response(ob_get_clean());
 }
@@ -19,7 +19,7 @@ $routes->add('hello',new Route('/hello/{name}',['name' => 'audry','_controller' 
 $routes->add('goodbye',new Route('/goodbye',['_controller' => 'render_template']));
 $routes->add('home',new Route('/',['_controller' => 'render_template' ]));
 // non-desirable side effect .. class is always instantiated
-$routes->add('world',new Route('/number',['_controller' => [new NumberController(), 'index'] ]));
+$routes->add('number',new Route('/number',['_controller' => 'App\Controller\NumberController::index' ]));
 
 
 return $routes;
